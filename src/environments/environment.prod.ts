69dacf571f836c7ed6c5cebb8fa46b521ef8c970
Carlos Josue <carlosjosuemn98@gmail.com>
2022-07-12 10:52:37 -0500

    feat: firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'redux-sales',
+    appId: '1:414870348884:web:822b012cc6d5709eeaf9c1',
+    storageBucket: 'redux-sales.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCbU-Ni7CxkbC8ztoBcjIZon96McbjNipo',
+    authDomain: 'redux-sales.firebaseapp.com',
+    messagingSenderId: '414870348884',
+  },
   production: true
 };
